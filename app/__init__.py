9f4d0654f4b70a60ebd6473b0b2c09950df8cba4
one-eyed-bat <mikolashuzaurus@gmail.com>
2024-05-27 23:56:50 +0100

    Registration and login

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
+login = LoginManager(app)
+login.login_view = 'login'
 db = SQLAlchemy()
 db.init_app(app)
 
--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,31 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+import sqlalchemy as sa
+from app import db
+from app.models import User
+
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
     submit = SubmitField('sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('register')
+
+    def validate_username(self, username):
+        user = db.session.scalar(sa.select(User).where(User.username == username.data))
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user_email = db.session.scalar(sa.select(User).where(User.email == email.data))
+        if user_email is not None:
+            raise ValidationError('Email address already registered.')
 
